d4dd762b27135847f0f7fa1d844a50e21ce5b21f
anjalikumari-9 <anjalikumari956939@gmail.com>
2024-10-15 20:03:48 +0530

    Add files via upload

---
+++ website.php
@@ -0,0 +1,219 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <style>
+        #a {
+            height: 30px;
+            width: 200px;
+            background: red(195, 244, 122);
+        }
+        .rounde-right{
+            transition: all 0.5s;
+        }
+        .rounded-right:hover{
+            transform: scale(1.2);
+            z-index:2;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 ">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                    <a class="navbar-brand " href="https://www.zomato.com/investor-relations ">Inventor relation</a>
+                    <button class="navbar-toggler " type="button " data-toggle="collapse " data-target="#navbarSupportedContent " aria-controls="navbarSupportedContent " aria-expanded="false " aria-label="Toggle navigation ">
+                        <span class="navbar-toggler-icon "></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse " id="navbarSupportedContent ">
+                        <ul class="navbar-nav mr-auto ">
+                            <li class="nav-item active ">
+                                <a class="nav-link " href="website.php">Home <span
+                                        class="sr-only ">(current)</span></a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link " href="login.php">Login</a>
+                            </li>
+
+                            <li class="nav-item ">
+                                <a class="nav-link disabled " href="prog.php">sign up</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0 ">
+                            <input class="form-control mr-sm-2 " type="search " placeholder="Search " aria-label="Search ">
+                            <button class="btn btn-outline-success my-2 my-sm-0 " type="submit ">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid ">
+        <div class="row justify-content-md-center ">
+            <div class="col-lg-12 ">
+                <img src="anj.jpeg " alt="... " class="rounded " height="500px " width="100% ">
+            </div>
+        </div>
+    </div><br />
+    <div class="container ">
+        <div class="row justify-content-md-center ">
+            <div class="col-lg-10 ">
+                <div class="card-group ">
+                    <div class="card m-2 border-1 p-2 rounded-3 " style="overflow: hidden; "><img class="card-img-top " src="img4.jpg " alt="Card image cap ">
+                        <div class="card-body ">
+                            <h5>Indian food</h5>
+                        </div>
+                    </div>
+                    <div class="card m-2 "><img class="card-img-top " src="img8.jpeg " alt="Card image cap " height="170px " width="300px ">
+                        <div class="card-body ">
+                            <h5>South indian</h5>
+                        </div>
+                    </div>
+                    <div class="card m-2 "><img class="card-img-top " src="img9.jpg " alt="Card image cap " height="170px " width="300px ">
+                        <div class="card-body ">
+                            <h5>Italic food</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container ">
+        <div class="row justify-content-md-center ">
+            <div class="col-sm-2 ">
+                <img src="img13.jpg " alt="... " class="rounded-right " height="200px " width="150px ">
+                <div class="carousel-caption d-none d-md-block">
+                    <p>EVERYDAY EATS</p>
+                    <a href="pastry.php">5places</a>
+                </div>
+            </div>
+            <div class=" col-sm-2 ">
+                <img src="img10.jpg " alt="... " class="rounded-right" height="200px " width="150px ">
+                <div class="carousel-caption d-none d-md-block ">
+                    <p>EVERYDAY EATS</p>
+                    <a href="roll.php">5places</a>
+
+                </div>
+            </div>
+            <div class=" col-sm-2 ">
+                <img class="card-img-top imgRel " src="img1..jpg " alt="card image cap " class="rounded-right " height="200px " width="150px ">
+                <div class="carousel-caption d-none d-md-block ">
+                    <p>EVERYDAY EATS</p>
+                    <a href="burger.php">5places</a>
+
+                </div>
+            </div>
+            <div class=" col-sm-2 ">
+                <img src="img14.jpg " alt="... " class="rounded-right " height="200px " width="150px ">
+                <div class="carousel-caption d-none d-md-block ">
+                    <p>EVERYDAY EATS</p>
+                    <a href="">5places</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br />
+    <div class="container-fluid ">
+        <div class="row justify-content-md-center " style=" height: 400px ; width:100%; background: rgb(248, 245, 245); ">
+            <div class=" col-sm-4 ">
+                <img src="phone " alt="... " class="rounded " height="300px " width="100% " style="margin-top: 20%; ">
+            </div>
+            <div class="col-sm-4 " style="margin-top: 5%; ">
+                <h3>Get the Zomato app</h3>
+                <p>We will send you a link, open it on your phone to download the app</p>
+                <input type="radio" name="radio" style=" height:  20px;width:20px; " />Email
+                <input type="radio" name="radio" style=" height: 20px;width:20px " />phone<br /><br />
+                <input placeholder="email " style="height:45px;width: 250px; " />
+                <button type=" button " class="btn btn-danger "><a href="# ">Share app link</a></button><br/><br/>
+                <p>download app from</p>
+                <img src="anj1.webp " height="40px " width="120px " />
+                <img src="anj2.webp " height="40px " width="120px " />
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+
+        <div class="row" style="height: 400px ; width:100%;background:rgb(221, 219, 219) ">
+
+            <div class="col-sm-2 ">
+                <h1><b>zomato</b></h1>
+                <h5>ABOUT ZOMATO</h5>
+                <a href="https://blog.zomato.com/">blog</a><br />
+                <a href="https://www.zomato.com/careers">works with us</a><br />
+                <a href="https://www.zomato.com/investor-relations">Investor relation</a><br />
+                <a href="https://www.zomato.com/report-fraud">Report froud</a><br />
+                <a href="https://blog.zomato.com/press-kit">Press kit</a>
+
+            </div>
+            <div class="col-sm-2">
+                <h5 style="margin-top: 60px;">ZOMAVERSE</h5>
+                <a href="website.html">zomato</a><br />
+                <a href="https://blinkit.com/">Blinkit</a><br />
+                <a href="https://www.feedingindia.org/">Freeding india</a><br />
+                <a href="https://www.hyperpure.com/">Hyperpure</a><br />
+                <a href="https://www.zomato.com/live">Zomato live</a><br />
+            </div>
+            <div class="col-sm-2">
+                <h5 style="margin-top: 60px;">FOR RESTORANT</h5>
+                <a href="https://www.zomato.com/partner-with-us">works with us</a><br />
+                <a href="https://play.google.com/store/apps/details?id=com.application.services.partner&hl=en_IN&gl=US">Apps for you</a><br />
+            </div>
+            <div class="col-sm-2">
+
+                <h5 style="margin-top: 60px;">LEARN MORE</h5>
+                <a href="https://www.zomato.com/policies/privacy/">Privacy</a><br />
+                <a href="https://www.zomato.com/policies/security/">Security</a><br />
+                <a href="https://www.zomato.com/policies/terms-of-service/">Terms</a><br />
+                <a href="https://www.zomato.com/directory">Sitemap</a>
+            </div>
+            <div class="col-sm-4" style="display: flex; align-items: center; flex-direction: column; gap: 20px;">
+                <div style="display: flex; gap: 20px;">
+                    <div id="a" style="height: 35px; width: 120px;border:1px solid black;margin-top: 10px; ">
+                        <img style="height:30px;width:40px;float: left; " src="flag-for-flag-india.svg " /><b>India</b>
+                    </div>
+                    <div id="a" style="height: 35px; width: 120px;border:1px solid black;margin-top: 10px; ">
+                        <img style="height:30px;width:40px;float: left; " src="global.svg" /><b>English</b>
+                    </div>
+                </div>
+
+                <div>
+                    <h5>SOCIAL LINKS</h5>
+                    <img src="linkedin.svg" heigh="23px" width="23px" />
+                    <img src="instagram.svg " height="23px " width="23px " />
+                    <img src="twitter.svg " height="23px " width="23px " />
+                    <img src="youtube.svg " height="23px " width="23px " />
+                    <img src="facebook.svg " height="23px " width="23px " />
+                    <br/><br/>
+                    <img src="anj1.webp " height="40px " width="150px " />
+                    <br/>
+                    <br/>
+                    <img src="anj2.webp " height="40px " width="150px " />
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
